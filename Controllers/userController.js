1bc2cab282b86dbcc184ac5be460009d46d38ac5
JayapriyaSiakumar <jayapriyaeswari05@gmail.com>
2025-12-18 14:17:08 +0530

    email url updated

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -86,7 +86,7 @@ export const forgotPassword = async (req, res) => {
       userDetail.email,
       "Password Reset link",
       `You are receiving this email because you have requested to reset your password.
-       Please click the following link to reset your password: https://localhost:5173/reset-password/${userDetail._id}/${token}
+       Please click the following link to reset your password: https://playful-naiad-52c608.netlify.app/reset-password/${userDetail._id}/${token}
        If you did not request this, please ignore this email.`
     );
     //console.log("Email Sent Successfully");
